8bbca689879310071830ad14fa23e4d2cf578cf3
JojoNowack <93148958+JojoNowack@users.noreply.github.com>
2021-11-02 18:03:48 +0100

    Update settings.py

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-print("johannes log os var: ",os.environ.get('IS_GITHUB'))
+print("johannes log var: ",os.environ.get('IS_GITHUB'))
 SECRET_KEY = config('SECRET_KEY')
 
 
