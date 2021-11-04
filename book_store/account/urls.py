011ba27174ad14c44e77ba8a8f14db5a9561d274
Kevlvin764 <Kevin.Schmidt@hs-augsburg.de>
2021-11-04 19:56:50 +0100

    alter functions and added dummy db for training usage

--- book_store/account/urls.py
+++ book_store/account/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-from account.views import loginView
+# account -> urls
 
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('login', loginView, name="login")
+# ]
 urlpatterns = [
-    path('login', loginView, name="login")
+    # the first page u see
+    path('', views.home, name='login-site'),
+    path('about/', views.about, name='about'),
 ]
--- book_store/account/views.py
+++ book_store/account/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-
 # Create your views here.
 
-def loginView(request):
-    return render(request, "login.html",{})
+posts = [
+    {
+        'author' : 'Kevin Schmidt',
+        'title' : 'Mein erstes Programm mit Django',
+        'content' : 'Das ist ein kurzer Überblick über das Framework',
+        'date_posted' : 'November 04, 2021'
+    },
+    {
+        'author' : 'Carmen Behringer',
+        'title' : 'Marketing ist nur Werbung',
+        'content' : 'Erläuterung des Marketing Begriffs',
+        'date_posted' : 'November 04, 2021'
+    }
+]
+# def loginView(request):
+#     return render(request, "login.html",{})
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'account/login.html', context)
+
+def about(request):
+    return render(request, 'account/about.html',{'title':'About'})
