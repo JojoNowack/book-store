db9100b55b56276045dbb1d177f8cbca01f059b5
Kevlvin764 <Kevin.Schmidt@hs-augsburg.de>
2021-11-09 14:40:01 +0100

    added the Profile Model to the admin

--- book_store/account/admin.py
+++ book_store/account/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Profile
 
 # Register your models here.
+# add the option for the admin, to have access to the data base manipulation
 admin.site.register(Post)
+admin.site.register(Profile)
