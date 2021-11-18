89d0afe6fa45fba37be076b5df9821b3dadbf6bc
JojoNowack <93148958+JojoNowack@users.noreply.github.com>
2021-11-18 22:26:33 +0100

    Update settings.py

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -32,7 +32,7 @@ if SECRET_KEY is None:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [bibforme.azurewebsites.net]
 
 
 # Application definition
