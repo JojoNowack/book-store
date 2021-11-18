37e1bc041bdc54c4b5e81992866c3fa6cb8c7948
JojoNowack <93148958+JojoNowack@users.noreply.github.com>
2021-11-18 22:11:01 +0100

    Update settings.py

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-print("johannes log var: ",)
+
 #SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') #try to get secret key from github
 if SECRET_KEY is None:
