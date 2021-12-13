def8f5d90017e1266623d7727a81702589a1aa48
Kevlvin764 <Kevin.Schmidt@hs-augsburg.de>
2021-12-13 14:08:33 +0100

    added dependencies between book and category

--- book_store/books/models.py
+++ book_store/books/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    genre = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.genre
+
 
 class Book(models.Model):
     book_img = models.ImageField(default = 'book.jpg', upload_to='book_pics')
@@ -11,7 +17,7 @@ class Book(models.Model):
     book_author = models.CharField(max_length=30)
     isavailable = models.BooleanField()
     quantity = models.IntegerField()
-    genre = models.CharField(max_length=12)
+    genre = models.ManyToManyField(Category)
     pages = models.IntegerField()
     part = models.IntegerField()
     age = models.IntegerField()
@@ -22,3 +28,5 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+
